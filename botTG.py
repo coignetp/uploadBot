f4fedbf1b779d82fa37d9d81ee793e331af47a04
Paul Coignet <paulcoignet1996@gmail.com>
2021-10-07 20:08:21 +0200

    USe python telegram bot

--- botTG.py
+++ botTG.py
@@ -1,7 +1,5 @@
-import sys
 import time
-import telepot
-from telepot.loop import MessageLoop
+import telegram
 import datetime
 import os
 import json
@@ -14,7 +12,7 @@ monthNames = ["01 -Janvier", "02 - Fevrier", "03 - Mars", "04 - Avril", "05 - Ma
 MainFolder = []
 
 
-def getFolder(yearfold, monthfold, dayfold):
+def getFolder(yearfold, monthfold):
     foldersY = google_drive_util.find_folders(yearfold)
 
     if len(foldersY) == 0:
@@ -30,27 +28,36 @@ def getFolder(yearfold, monthfold, dayfold):
     return foldersM[-1]
 
 
-def uploadToDrive(fname, yearfold, monthfold, dayfold):
-    folder = getFolder(yearfold, monthfold, dayfold)
+def uploadToDrive(fname, yearfold, monthfold):
+    folder = getFolder(yearfold, monthfold)
     google_drive_util.upload_files_to_folder([fname], folder)
 
 
-def handle(msg):
-    content_type, chat_type, chat_id = telepot.glance(msg)
+def handle(bot, msg):
+    # content_type, chat_type, chat_id = telepot.glance(msg)
+
+    file = None
+
+    try:
+        # Try photo first
+        file = bot.getFile(msg.photo[0].file_id)
+        print('Found a photo')
+    except Exception:
+        try:
+            # Video second
+            file = bot.getFile(msg.video.file_id)
+            print('Found a video')
+        except Exception:
+            return
 
     now = datetime.datetime.now()
-    if content_type == 'photo' or content_type == 'video':
-
-        print(msg[content_type])
-        fl = bot.getFile(msg[content_type][-1]['file_id'])
-
+    if file != None:
         filename = 'img_' + str(now.hour) + '-' + str(now.minute) + '-' + str(
-            now.second) + '-' + str(now.microsecond) + '.' + fl['file_path'].split('.')[-1]
+            now.second) + '-' + str(now.microsecond) + '.' + file.file_path.split('.')[-1]
 
-        bot.download_file(msg[content_type][-1]['file_id'], filename)
+        file.download(filename)
 
-        uploadToDrive(filename, 'annee_' + str(now.year),
-                      monthNames[now.month - 1], 'jour_' + str(now.day))
+        uploadToDrive(filename, 'annee_' + str(now.year), monthNames[now.month - 1])
 
         os.remove(filename)
 
@@ -71,9 +78,16 @@ if __name__ == '__main__':
         MainFolder = google_drive_util.find_folders(FOLDER)[-1]
 
         # The bot is ready to start
-        bot = telepot.Bot(TOKEN)
-        MessageLoop(bot, handle).run_as_thread()
+        bot = telegram.Bot(TOKEN)
         print('Listening ...')
 
         while 1:
             time.sleep(10)
+            # Get all the messages read since last time
+            try:
+                updates = bot.get_updates(limit=1000, timeout=10, allowed_updates=['message'])
+
+                for msg in updates:
+                    handle(bot, msg.message)
+            except Exception as e:
+                print(e)
